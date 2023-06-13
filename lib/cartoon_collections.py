f419020a88071177df82020a188c022f605d1105
Shams <skm478@gmail.com>
2023-06-13 11:37:44 -0400

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarf_list):
+    for idx, val in enumerate(dwarf_list):
+        print(f"{idx+1}. {val}")
     pass
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_call):
+    new_list = []
+    for val in planeteer_call:
+        new_list.append(f"{val.capitalize()}!")
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for val in calls:
+        if len(val) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(str_list):
+    for val in str_list:
+        if val in ['cheddar', 'gouda', 'camembert']:
+            return val
+    return None
